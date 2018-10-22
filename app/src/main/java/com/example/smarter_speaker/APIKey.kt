768779a5eeec59c8f08fd4f985dc1b57dd519fe8
cassiechen <cassiechen@kkbox.com>
2018-10-22 16:23:14 +0800

    init app

---
+++ app/src/main/java/com/example/smarter_speaker/APIKey.kt
@@ -0,0 +1,7 @@
+package com.example.smarter_speaker
+
+object APIKey {
+    val olamiKey = "3b2843a4a9fb4bf1939f1bf64a635001"
+    val olamiSecret = "62d1237bc80049c28f121068c7693c65"
+    val TTSKey = "fa4060523fdb40a69074efe1de9bddbb"
+}
